f14e9796e2286ebaf28e658c62c7c1029d196a95
Mohammed-Alramahi <vittosc1997@gmail.com>
2021-04-04 01:13:22 +0300

    js file refactored

--- js/app.js
+++ js/app.js
@@ -5,15 +5,12 @@ let userName = prompt('Welcome to my website, Type your name below so we can kno
 alert('Hello ' + userName + ' I hope you are doing great');
 alert('I\'d like to play a game If you guess right then you rock!, answer with yes/y or no/n');
 
-//question #1
-
 function question1() {
 
   let myHobby = prompt('Do you think gaming is my favourite hobby?').toLowerCase();
   while (myHobby !== 'yes' && myHobby !== 'y' && myHobby !== 'no' && myHobby !== 'n') {
     myHobby = prompt('try typing yes/y or no/n only please!').toLowerCase();
   }
-  //console.log(userName + ' answered: ' + myHobby + ' for the first question');
   if (myHobby === 'yes' || myHobby === 'y') {
     alert('You guessed right, I mean who doesn\'t like gaming');
     userPoints++;
@@ -25,14 +22,11 @@ function question1() {
 question1();
 
 
-
-//question #2
 function question2() {
   let isCool = prompt('Do you think I\'m cool? answer with yes or I\'ll kill you :p').toLowerCase();
   while (isCool !== 'yes' && isCool !== 'y' && isCool !== 'no' && isCool !== 'n') {
     isCool = prompt('try typing yes/y or no/n only please!').toLowerCase();
   }
-  //console.log(userName + ' answered: ' + isCool + ' for the second question');
   if (isCool === 'yes' || isCool === 'y') {
     userPoints++;
     alert('of course I\'m cool, thx a lot <3 ');
@@ -44,13 +38,12 @@ function question2() {
 }
 question2();
 
-//question #3
+
 function question3() {
   let programmingPrefer = prompt('Would I rather to be programmer instead of being rich?').toLowerCase();
   while (programmingPrefer !== 'yes' && programmingPrefer !== 'y' && programmingPrefer !== 'no' && programmingPrefer !== 'n') {
     programmingPrefer = prompt('try typing yes/y or no/n only please!').toLowerCase();
   }
-  //console.log(userName + ' answered: ' + programmingPrefer + ' for the third question');
 
   if (programmingPrefer === 'yes' || programmingPrefer === 'y') {
     alert('Wrong answer, I like programming but I like money more :D');
@@ -62,14 +55,12 @@ function question3() {
 }
 question3();
 
-//question #4
+
 function question4() {
   let favLaunch = prompt('Is mansaf my favourite thing to eat? If I was you, I\'ll totally answer yes').toLowerCase();
   while (favLaunch !== 'yes' && favLaunch !== 'y' && favLaunch !== 'no' && favLaunch !== 'n') {
     favLaunch = prompt('try typing yes/y or no/n only please!').toLowerCase();
   }
-  //console.log(userName + ' answered: ' + favLaunch + ' for the third question');
-
   if (favLaunch === 'yes' || favLaunch === 'y') {
     userPoints++;
     alert('specially on friday.. yummy!');
@@ -81,13 +72,11 @@ function question4() {
 question4();
 
 
-//question #5
 function question5() {
   let isOld = prompt('Am I old to be a here?').toLowerCase();
   while (isOld !== 'yes' && isOld !== 'y' && isOld !== 'no' && isOld !== 'n') {
     isOld = prompt('try typing yes/y or no/n only please!').toLowerCase();
   }
-  //console.log(userName + ' answered: ' + isOld + ' for the last question');
   if (isOld === 'yes' || isOld === 'y') {
     alert('Is 23 years age considered old? weird!!');
   }
@@ -100,7 +89,6 @@ function question5() {
 }
 question5();
 
-//question #6
 function question6() {
   alert('You are no doing bad so far, This one should be easy for you');
   let userNumberInput = prompt('on a scale of 1 to 10 what do you think about me?');
@@ -125,19 +113,15 @@ function question6() {
       alert('that question was very simple but unfotunately\n you couldn\'t manage to guess 10 -_-');
     }
   }
-
-
 }
 question6();
 
-//question #7
 function question7() {
   let games = ['gta', 'csgo', 'dirt', 'pes', 'fifa'];
   let userInput = null;
   let flag = false;
   let tries = 5;
   Tries: while (tries >= 0) {
-
     userInput = prompt('can you guess my favourite games?').toLowerCase();
     for (let i = 0; i < games.length; i++) {
       if (userInput === games[i]) {
@@ -158,8 +142,6 @@ function question7() {
       alert('You didn\'t answer correctly');
     }
     tries--;
-
-
   }
   if (flag) {
     alert('You are correct my dear');
@@ -168,6 +150,7 @@ function question7() {
     alert(`The correct answer was one of those ${games}`);
   }
 }
+
 question7();
 if (userPoints >= 4 && userPoints < 6) {
   document.getElementById('intro-header').innerHTML = `That was not bad,  ${userName} you got ${userPoints} out of 7`;
